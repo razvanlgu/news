5e05f409443525f5e6bc648db9c1cfe1ffdbddae
razvan.lungu <razvan.lungu@teamnet.ro>
2019-07-09 17:51:10 +0300

    added the redux files

---
+++ lib/redux/reducer.dart
@@ -0,0 +1,10 @@
+/****
+    De adaugat import pentru fiecare reducer
+ ***/
+
+AppState appReducer(AppState state, dynamic action) =>
+    AppState(
+      /****
+       * De adaugat toate stateurile cu propriul reducer
+       */
+    );
